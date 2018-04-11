428fcac5ea3b40654631fd0aaaeb66c2c95bee68
rna88 <pool@localhost.localdomain>
2018-04-11 02:15:22 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,28 @@
+## Valve Player Query
+This python script will show notifications of increasing online player count for multiplayer games hosted with the valve master server. This is only useful for games with a low player count that have sporadic activity; you can be informed when people are playing without leaving the game running or needing to check a website.
+
+## Usage for Linux
+#### Dependencies
+You will need [python-valve], [libnotify], and of course the python 2 or 3 packages installed.
+```
+sudo pip install python-valve
+sudo pacman -S libnotify
+```
+
+#### Running
+Basic usage: `python valvePlayerQuery <gamedir> <minPlayers> <refreshRate> <retries>`
+
+<gamedir>       the ID string of the game, found on https://steamdb.info under "Additional Information"
+<minPlayers>    Minimum player count before we display notification
+<refreshRate>   Minutes before querying the master server again
+<retries>       Number of times to retry connecting to the master server on timeout
+
+Optionally you may place a small image called <gamedir>.png in the same location as the script. This image will appear on the notification, allowing you to easily identify different notifications for different games.
+
+#### Example
+Demonstrating usage for fistful of frags:
+
+`setsid python valvePlayerQuery.py fof 20 10 3 >/dev/null 2>&1 &`
+
+[python-valve]:(https://github.com/serverstf/python-valve)
+[libnotify]:(https://wiki.archlinux.org/index.php/Desktop_notifications#Libnotify)
---
+++ valvePlayerQuery.py
@@ -0,0 +1,85 @@
+import subprocess
+import gi
+gi.require_version('Notify', '0.7')
+from gi.repository import Notify, GdkPixbuf
+import valve.source.master_server
+import time
+import socket
+import os
+import sys
+
+totalPlayers = 0
+lastPlayers = 0
+timeout = 0
+
+if len(sys.argv) < 5:
+	print("Usage: python valvePlayerQuery <gamedir> <minPlayers> <refreshRate> <retries>\n\n"
+	+"<gamedir>       the ID string of the game, found on https://steamdb.info under \"Additional Information\"\n"
+	+"<minPlayers>    Minimum player count before we display notification\n"
+	+"<refreshRate>   Minutes before querying the master server again\n"
+	+"<retries>       Number of times to retry connecting to the master server on timeout")
+	sys.exit()
+
+gameDir = sys.argv[1]
+minPlayers = int(sys.argv[2])
+refreshRate = int(sys.argv[3])
+retries = int(sys.argv[4])
+
+Notify.init("valvePlayerQuery")
+notifyPlayers = Notify.Notification.new("You shouldn't be seeing this")
+notifyTimeout = Notify.Notification.new("Timeout")
+
+# Use a custom image for the notifications if we find one. 
+if os.path.isfile(gameDir+".png"):
+	image = GdkPixbuf.Pixbuf.new_from_file(gameDir+".png")
+	notifyPlayers.set_icon_from_pixbuf(image)
+	notifyPlayers.set_image_from_pixbuf(image)
+	notifyTimeout.set_icon_from_pixbuf(image)
+	notifyTimeout.set_image_from_pixbuf(image)
+else:
+	image = GdkPixbuf.Pixbuf.new_from_file("default.png")
+	notifyPlayers.set_icon_from_pixbuf(image)
+	notifyPlayers.set_image_from_pixbuf(image)
+	notifyTimeout.set_icon_from_pixbuf(image)
+	notifyTimeout.set_image_from_pixbuf(image)
+
+while True:
+	# Check if the game is running, if it is don't bother displaying a notification.
+	playing = subprocess.call("ps -ef | grep "+gameDir+" | grep -vE '$$|"+sys.argv[0]+"'",stderr=subprocess.STDOUT,shell=True) 
+	
+	if playing == 1:
+		msq = valve.source.master_server.MasterServerQuerier()
+		try:
+			for address in msq.find(region=[u"na", u"eu", u"sa", u"as",u"oc",u"af"], gamedir = gameDir):
+				server = valve.source.a2s.ServerQuerier(address)
+				info = server.get_info()["server_name"]
+				playerCount  = server.get_players()["player_count"]
+				try:
+					playerCount
+				except NameError:
+					playercount = 0
+				print(playerCount, info)
+				totalPlayers += playerCount
+			timeout = 0
+
+		# Try reconnecting n times, if unsuccessful show timeout notification and sleep until next cycle.
+		except (valve.source.a2s.NoResponseError, socket.error) as e:
+			timeout += 1
+			# Player count is reset on timeout for next request.
+			playerCount = 0
+			if timeout >= retries:
+				notifyTimeout.show()
+				print("Master server request timed out!")
+				timout = 0
+				time.sleep(refreshRate*60)
+			continue
+
+	if (totalPlayers >= minPlayers) & (totalPlayers != lastPlayers):
+		lastPlayers = totalPlayers
+		notifyPlayers.update( str(totalPlayers) + " online")
+		notifyPlayers.show()
+		totalPlayers = 0
+	else: 
+		pass
+
+	time.sleep(refreshRate*60)	# convert refreshRate to seconds and sleep.
